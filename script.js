b76c5e870e74921a016fab8184f6d1f54e14390d
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-25 12:39:21 +0000

    script

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const expenseForm = document.getElementById("expense-form");
-  const expenseNameInput = document
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+
+  let expenses = [];
+  let totalAmount = calculateTotal();
+
+  expenseForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name =  expenseNameInput.value.trim();
+
+  })
+
+  function calculateTotal(){ 
+    let total = 0;
+  }
+
 })
